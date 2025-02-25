1ed222354ce819713e4bf5a4fdf670c425753826
Beto-droid <alberto.javier.magrini@gmail.com>
2025-02-24 21:07:40 -0300

    migration and forgot url

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('users/', views.UserListView.as_view()),
 ]
+
+router = DefaultRouter()
+router.register('tasks', views.TaskViewSet)
+urlpatterns += router.urls
